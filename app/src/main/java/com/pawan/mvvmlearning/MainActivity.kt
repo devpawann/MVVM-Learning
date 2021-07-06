646893923796ac76e8072554277ca755a7fd4176
Pawan Acharya <pawanacharya070@gmail.com>
2021-07-06 08:19:50 +0545

    simple data binding with greeting example

--- app/src/main/java/com/pawan/mvvmlearning/MainActivity.kt
+++ app/src/main/java/com/pawan/mvvmlearning/MainActivity.kt
@@ -2,10 +2,19 @@ package com.pawan.mvvmlearning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.pawan.mvvmlearning.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btnSubmit.setOnClickListener {
+            val name:String=binding.etName.text.toString()
+            binding.tvGreeting.text="Hello $name"
+        }
+
     }
 }
