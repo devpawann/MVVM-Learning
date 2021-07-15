c212a8f905b04e93aa129b4d0a5c622dad87f1ae
Pawan Acharya <pawanacharya070@gmail.com>
2021-07-15 10:41:48 +0545

    query parameters

--- app/src/main/java/com/pawan/mvvmlearning/AlbumsService.kt
+++ app/src/main/java/com/pawan/mvvmlearning/AlbumsService.kt
@@ -2,9 +2,13 @@ package com.pawan.mvvmlearning
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface AlbumsService {
 
     @GET("/albums")
-    suspend fun getAlbums():Response<Album>
+    suspend fun getAlbums(): Response<Album>
+
+    @GET("/albums")
+    suspend fun getAlbumsOfSingleUserId(@Query("userId") userId: Int): Response<Album>
 }
--- app/src/main/java/com/pawan/mvvmlearning/MainActivity.kt
+++ app/src/main/java/com/pawan/mvvmlearning/MainActivity.kt
@@ -3,28 +3,34 @@ package com.pawan.mvvmlearning
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.liveData
+import com.pawan.mvvmlearning.databinding.ActivityMainBinding
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val retrofitService =
             RetrofitInstance.getRetrofitInstance().create(AlbumsService::class.java)
 
-        val responseLiveData:LiveData<Response<Album>> = liveData {
-            val response=retrofitService.getAlbums()
+        val responseLiveData: LiveData<Response<Album>> = liveData {
+            //val response = retrofitService.getAlbums()
+            val response = retrofitService.getAlbumsOfSingleUserId(3)
             emit(response)
         }
 
         responseLiveData.observe(this, Observer { it ->
-            val albumsList=it.body()
-            albumsList?.forEach{
-             Log.d("TAG",it.toString())
+            val albumsList = it.body()
+            albumsList?.forEach {
+                Log.d("TAG", it.toString())
+
+                binding.tvAlbum.append(it.toString()+"\n\n")
             }
         })
     }
